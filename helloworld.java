843eb6917ddf8183ac40992c36066a4eac2262b5
Deepak667 <deepak.bhor7@gmail.com>
2025-08-23 13:45:04 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("to test the secondddddd buiddddddld for process");
+     System.out.println("to test the secondddd266522222dd buiddddddld for process");
       System.out.println("Test the seconf buisdfsdfsfsfsfsfsdfld process");
     }  
 } 
