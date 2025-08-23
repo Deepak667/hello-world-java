237677ecb39edb8394a24493f3a63d2fa7a65660
Deepak667 <deepak.bhor7@gmail.com>
2025-08-23 13:34:51 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("to test the second build");
+     System.out.println("to test the second build for process");
       System.out.println("Test the seconf build process");
     }  
 } 
