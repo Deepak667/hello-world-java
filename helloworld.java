4635518388db49bab7e6bad4940fc8d9bea149fe
Deepak667 <deepak.bhor7@gmail.com>
2025-12-24 09:24:39 +0530

    Rename class Simple to helloworld

--- helloworld.java
+++ helloworld.java
@@ -1,4 +1,4 @@
-class Simple{  
+class helloworld{  
     public static void main(String args[]){  
      System.out.println("to test the secondddd266522222dd buiddddddld for process");
       System.out.println("Test the seconf buisdfsdfsfsfsfsfsdfld process");
