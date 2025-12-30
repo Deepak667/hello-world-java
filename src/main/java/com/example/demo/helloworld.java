b96d51abd695cadb9fda08c96a4e063f40d0695c
Deepak <deepak.bhor7@gmail.com>
2025-12-30 15:58:06 +0530

    Refactor project structure: replace build.gradle with pom.xml, update Dockerfile to copy target jar, and restore helloworld class

--- src/main/java/com/example/demo/helloworld.java
+++ src/main/java/com/example/demo/helloworld.java
@@ -1,12 +1,13 @@
 @SpringBootApplication
 @RestController
-public class App {
+public class helloworld {
+
   @GetMapping("/")
   public String hello() {
     return "Hello from Kubernetes";
   }
 
   public static void main(String[] args) {
-    SpringApplication.run(App.class, args);
+    SpringApplication.run(DemoApplication.class, args);
   }
 }
