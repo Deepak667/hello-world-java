165075b385bd06645cc813c5a33c9f51abd7f21e
Deepak667 <deepak.bhor7@gmail.com>
2025-12-30 15:37:50 +0530

    Migrate helloworld to Spring Boot application
    Refactored helloworld.java to use Spring Boot framework.

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,12 @@
-class helloworld{  
-    public static void main(String args[]){  
-     System.out.println("to test the secondddd266522222dd buiddddddld for process");
-      System.out.println("Test the seconf buisdfsdfsfsfsfsfsdfld process");
-    }  
-} 
+@SpringBootApplication
+@RestController
+public class App {
+  @GetMapping("/")
+  public String hello() {
+    return "Hello from Kubernetes";
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(App.class, args);
+  }
+}
