8c90f22ca717dee892700db5e0b32b018e596663
Deepak667 <deepak.bhor7@gmail.com>
2026-01-07 20:29:10 +0530

    Update helloworld.java

--- src/main/java/com/example/demo/helloworld.java
+++ src/main/java/com/example/demo/helloworld.java
@@ -11,7 +11,7 @@ public class helloworld {
 
   @GetMapping("/")
   public String hello() {
-    return "Hello from Kubernetes version 12.....";
+    return "Hello from Kubernetes version 13.....";
   }
 
   public static void main(String[] args) {
